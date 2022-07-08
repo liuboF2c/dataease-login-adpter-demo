10e34d22c3c28b21e89b81eaf25b1ca3b430680d
liubo <liubo@fit2cloud.com>
2022-07-08 15:37:22 +0800

    remove jackson

--- src/main/java/dataease/lboo/demo/controller/DataEaseLoginController.java
+++ src/main/java/dataease/lboo/demo/controller/DataEaseLoginController.java
@@ -1,7 +1,6 @@
 package dataease.lboo.demo.controller;
 
 import com.alibaba.fastjson.JSONObject;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import dataease.lboo.demo.util.EncryptUtils;
 import dataease.lboo.demo.util.HttpClientConfig;
 import dataease.lboo.demo.util.HttpClientUtil;
